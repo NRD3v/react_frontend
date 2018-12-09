993457c2534d9052e9bb80ed15988ed6abbdcbb4
NRD3v <nr.dev.contact@gmail.com>
2018-12-09 10:11:31 +0300

    Refacto DoctorComponent: lightening

--- src/components/DoctorComponent.js
+++ src/components/DoctorComponent.js
@@ -1,49 +1,29 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {fetchLinks} from "../store/actions/links";
-import {fetchDoctors, resetDoctors} from "../store/actions/doctors";
+import React from "react";
 import {Card, CardBody, Col, Row} from "reactstrap";
 
-class DoctorComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.onClick = this.onClick.bind(this);
-    }
-
-    onClick() {
-    }
-
-    render() {
-        return (
-            <Card className={'col-6 p10'}>
-                <CardBody>
-                    <Row>
-                        <Col className={'col-4'}>
-                            <img src={require('../images/Photo.jpg')} className="imageRadius2" width="100" />
-                        </Col>
-                        <Col className={'col-8 card-text h-50 maxh50'}>
-                            <span>{this.props.doctor.name}</span>
-                            <br/>
-                            <span className={'grey'}>{this.props.doctor.city}</span>
-                            <br/>
-                            <span className={'small'}>
-                                {this.props.doctor.practice.map((practice, index) => {
-                                    return <span key={index}>{ (index ? ', ' : '') + practice}</span>
-                                })}
-                            </span>
-                            <br/>
-                            <span className={'small font-weight-bold text-primary'}>Prendre RDV</span>
-                        </Col>
-                    </Row>
-                </CardBody>
-            </Card>
-        )
-    }
+export default function DoctorComponent({doctor}) {
+    return (
+        <Card className={'col-6 p10'}>
+            <CardBody>
+                <Row>
+                    <Col className={'col-4'}>
+                        <img src={require('../images/Photo.jpg')} className="imageRadius2" alt="" width="100" />
+                    </Col>
+                    <Col className={'col-8 card-text h-50 maxh50'}>
+                        <span>{doctor.name}</span>
+                        <br/>
+                        <span className={'grey'}>{doctor.city}</span>
+                        <br/>
+                        <span className={'small'}>
+                            {doctor.practice.map((practice, index) => {
+                                return <span key={index}>{(index ? ', ' : '') + practice}</span>
+                            })}
+                        </span>
+                        <br/>
+                        <span className={'small font-weight-bold text-primary'}>Prendre RDV</span>
+                    </Col>
+                </Row>
+            </CardBody>
+        </Card>
+    )
 }
-
-const mapStateToProps = state => ({
-    doctors: state.doctors.list,
-    links: state.links.list
-});
-
-export default connect(mapStateToProps, {fetchLinks, fetchDoctors, resetDoctors})(DoctorComponent);
